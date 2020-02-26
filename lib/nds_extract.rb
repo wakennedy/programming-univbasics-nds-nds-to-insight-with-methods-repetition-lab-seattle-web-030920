2f67cd2d63231e5e4daf495063cf7e31e9829b59
William Kennedy <alexkenned@gmail.com>
2020-02-26 19:37:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,7 @@ def total_gross(source)
   total = 0 
   i = 0 
   while i < source.length do 
-    total += directors_totals[list_of_directors][0]
+    total += directors_totals[list_of_directors[0]][0]
     i+=1 
   end
   total
